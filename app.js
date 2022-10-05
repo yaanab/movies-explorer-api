8b56c1924b763362f9161fca381fea4927cff580
yaana <ya.anastasya.b@yandex.ru>
2022-10-05 17:00:12 +0300

    fix: удален crash test

--- app.js
+++ app.js
@@ -3,25 +3,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 const routes = require('./routes/index');
 const { errorFunction } = require('./middlewares/error-function');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const crashTest = require('./routes/crash-test');
 const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
+app.use(helmet());
 app.use(cors);
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
-app.use(crashTest);
 app.use(routes);
 app.use(errorLogger);
 app.use(errors());
