84e22bdfd3055c8040ec90ca2d9afdc5b05d1027
yaana <ya.anastasya.b@yandex.ru>
2022-10-06 11:49:04 +0300

    fix: ошибки линтера

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 module.exports.limiter = rateLimit({
-	windowMs: 15 * 60 * 1000,
-	max: 100,
-	standardHeaders: true,
-	legacyHeaders: false,
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
 });
