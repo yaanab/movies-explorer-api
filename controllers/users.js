4bb21bd181132b5e6e771eb12115f35b868acb3f
yaana <ya.anastasya.b@yandex.ru>
2022-11-05 17:08:59 +0300

    fix: исправлено удаление фильма

--- controllers/users.js
+++ controllers/users.js
@@ -28,9 +28,9 @@ module.exports.createUser = (req, res, next) => {
       res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
-          sameSite: 'none',
-          secure: true,
+          // httpOnly: true,
+          // sameSite: 'none',
+          // secure: true,
         });
       res.send({ token });
       // }
@@ -90,9 +90,9 @@ module.exports.login = (req, res, next) => {
       res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
-          sameSite: 'none',
-          secure: true,
+          // httpOnly: true,
+          // sameSite: 'none',
+          // secure: true,
         });
       res.send({ token });
     })
